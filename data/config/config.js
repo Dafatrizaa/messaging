39b1e3542e37bd414dea593abb995f0ec8478810
Dz <95111600+Dafatrizaa@users.noreply.github.com>
2022-02-19 16:43:44 +0700

    Update config.js

--- data/config/config.js
+++ data/config/config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "GANTI DENGAN PROJECT KALIAN",
-    authDomain: "GANTI DENGAN PROJECT KALIAN",
-    databaseURL: "GANTI DENGAN PROJECT KALIAN",
-    projectId: "GANTI DENGAN PROJECT KALIAN",
-    storageBucket: "GANTI DENGAN PROJECT KALIAN",
-    messagingSenderId: "GANTI DENGAN PROJECT KALIAN",
-    appId: "GANTI DENGAN PROJECT KALIAN"
+  apiKey: "AIzaSyDr64U6WGVwNITpbU0j4N0KwOd7ZRGwCmQ",
+  authDomain: "healthy-icon-339511.firebaseapp.com",
+  databaseURL: "https://healthy-icon-339511-default-rtdb.firebaseio.com",
+  projectId: "healthy-icon-339511",
+  storageBucket: "healthy-icon-339511.appspot.com",
+  messagingSenderId: "17053748826",
+  appId: "1:17053748826:web:896ab731bcfacb91e01263"
 };
 const app = firebase.initializeApp(firebaseConfig);
